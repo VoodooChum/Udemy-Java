9eecd8a41f16c3f3f350dc7c88dde6a547bbb8ac
brimill <brian.miller.ext@sodexo.com>
2021-08-04 15:24:13 -0500

    Added class files to gitIgnore

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,19 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Brian");
+        String hello = "Hello";
+        String name = "Brian";
+        System.out.println(hello + " " + name);
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("My first number: " + myFirstNumber);
+        System.out.println("My second number: " + mySecondNumber);
+        System.out.println("My third number: " + myThirdNumber);
+        System.out.println("My total: " + myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println("My last one: " + myLastOne);
     }
 }
